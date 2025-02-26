aa61faa19a98279df5d0f2fd0d8d81d7edba7043
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2025-02-26 15:40:37 -0700

    set up project, started on header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,7 +3,13 @@ import './header.scss'
 export default function Header() {
  return (
   <div className='header'>
-   <p>header</p>
+   <div className='mobile-search'>
+    <button className='start-search-mobile'>
+     <img src='/assets/asset 65.svg' />
+     Start your search
+    </button>
+   </div>
+   <div className='sidescroll-menu'> i am the sidescroll menu</div>
   </div>
  )
 }
