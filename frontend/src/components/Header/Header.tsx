8faa985c47672ad73c4845ca25e98e37014d9718
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2025-02-27 16:28:19 -0700

    styled mobile header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,15 +1,29 @@
 import './header.scss'
 
 export default function Header() {
+ const homeTypes = ['OMG!', 'Icons', 'Play', 'Off-the-grid', 'Top cities', 'National parks', 'Cabins', 'Treehouses', 'Beachfront', 'Mansions', 'Castles', 'Amazing Views', 'Countryside', 'New', 'Houseboats', 'Earth homes', 'Domes', 'Caves', 'Historical homes', 'Lake', 'A-frames', 'Lakefront', 'Farms', 'Luxe', 'Trending', 'Islands', 'Dammusi', 'Containers', 'Riads', 'Golfing', 'Hanoks', 'Amazing pools', 'Desert', 'Boats', 'Cycladic homes', 'Camping', 'Rooms', 'Ski in/out', 'Creative spaces', 'Yurts', 'Design', 'Tropical', 'Tiny homes', 'Campers', 'Arctic', 'Surfing', 'Skiing', 'Vineyards', 'Bed & breakfasts', "Chef's kitchens", "Shepherd's huts", 'Top of the world', 'Ryokans', 'Minsus', 'Barns', 'Casas particulares', 'Windmills', 'Grand pianos', 'Towers', 'Adapted', 'Trulli', 'Beach']
+
+ const images = Array.from({ length: 62 }, (_, index) => `/assets/asset ${index}.jpeg`)
+
  return (
   <div className='header'>
-   <div className='mobile-search'>
+   <div className='mobile-header'>
     <button className='start-search-mobile'>
      <img src='/assets/asset 65.svg' />
      Start your search
     </button>
+    <div className='sidescroll-menu'>
+     <ul>
+      {images.map((src, index) => (
+       <li key={index}>
+        <img src={src} alt={`Asset ${index}`} />
+        <p>{homeTypes[index]}</p> {/* Display corresponding text */}
+       </li>
+      ))}
+     </ul>
+    </div>
    </div>
-   <div className='sidescroll-menu'> i am the sidescroll menu</div>
+
   </div>
  )
 }
