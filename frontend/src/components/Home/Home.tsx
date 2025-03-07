b447ac18249a10bcc90914db73b6cf8d7437b2fc
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2025-03-07 16:30:28 -0700

    worked on slider under header for mobile

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -7,6 +7,18 @@ export default function Home() {
  return (
   <div className='home-main-container'>
    <Header />
+   <div className='total-price-slider-container'>
+    <div className='total-price-slider-desc'>
+     <h1>Display total price</h1>
+     <p>Includes all fees, before taxes</p>
+    </div>
+    <div>
+     <label className="switch">
+      <input type="checkbox"></input>
+      <span className="slider round"></span>
+     </label>
+    </div>
+   </div>
    <p>Home page</p>
   </div>
  )
