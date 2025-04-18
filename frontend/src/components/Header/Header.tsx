f309b8dea2c438b43c1843015e2b5ff8dae76953
Jessica Pabst <jess@Lorens-MacBook-Pro.local>
2025-04-18 16:56:41 -0600

    working on transform of mid size header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,34 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './Header.scss';
 
 export default function Header() {
  const homeTypes = ['OMG!', 'Icons', 'Play', 'Off-the-grid', 'Top cities', 'National parks', 'Cabins', 'Treehouses', 'Beachfront', 'Mansions', 'Castles', 'Amazing Views', 'Countryside', 'New', 'Houseboats', 'Earth homes', 'Domes', 'Caves', 'Historical homes', 'Lake', 'A-frames', 'Lakefront', 'Farms', 'Luxe', 'Trending', 'Islands', 'Dammusi', 'Containers', 'Riads', 'Golfing', 'Hanoks', 'Amazing pools', 'Desert', 'Boats', 'Cycladic homes', 'Camping', 'Rooms', 'Ski in/out', 'Creative spaces', 'Yurts', 'Design', 'Tropical', 'Tiny homes', 'Campers', 'Arctic', 'Surfing', 'Skiing', 'Vineyards', 'Bed & breakfasts', "Chef's kitchens", "Shepherd's huts", 'Top of the world', 'Ryokans', 'Minsus', 'Barns', 'Casas particulares', 'Windmills', 'Grand pianos', 'Towers', 'Adapted', 'Truli', 'Beach']
  const [focusedIndex, setFocusedIndex] = useState(null);
+ const [isScrollingDown, setIsScrollingDown] = useState(false);
+ const [lastScrollY, setLastScrollY] = useState(0);
+
+
+ function handleFocus(index) {
+  setFocusedIndex(index);
+ }
+
+ function handleBlur() {
+  setFocusedIndex(null);
+ }
+
+ useEffect(() => {
+  const handleScroll = () => {
+   const scrollY = window.scrollY;
+
+   setIsScrollingDown(scrollY > lastScrollY);
+   setLastScrollY(scrollY);
+  };
+
+  window.addEventListener("scroll", handleScroll);
+  return () => window.removeEventListener("scroll", handleScroll);
+ }, [lastScrollY]);
+
+
 
  const images = Array.from({ length: 62 }, (_, index) => `/assets/asset ${index}.jpeg`)
 
@@ -26,7 +51,7 @@ export default function Header() {
     </div>
    </div>
 
-   <div className='mid-header'>
+   <div className={`mid-header`}>
     <div className='top-row'>
      <img className='logo' src='/assets/asset 63.svg' />
      <div className='header-right-container'>
@@ -39,45 +64,65 @@ export default function Header() {
      </div>
     </div>
 
-    <div className='middle-row'>
+    <div className={`middle-row ${isScrollingDown ? "fade-out-and-up" : ""}`}>
      {/* these toggle colors and change the bottom row */}
      <p>Homes</p>
      <p>Experiences</p>
     </div>
 
-    <div className='bottom-row'>
+    <div className={`bottom-row ${isScrollingDown ? "merge-up" : ""}`}>
      <div className='bottom-row-content-container'>
 
-      {/* need to change these to inputs instead -- types -- calendar, text. li won't work for the functionality we are looking for */}
-      <div className='search-criteria-container'>
-       <div className='first-div div-hover search-criteria' tabIndex={-1}>
+
+      <div className={`search-criteria-container ${focusedIndex !== null ? 'container-dimmed' : ''}`}>
+       <div
+        className={`first-div div-hover search-criteria ${focusedIndex === 0 ? 'focused-div' : 'unfocused-div'}`}
+        onFocus={() => handleFocus(0)}
+        onBlur={handleBlur}
+        tabIndex={0}
+       >
         <p className='heading'>Where</p>
         <input className='sub-heading' placeholder='Search destinations' />
        </div>
        <div>
         <div className='divider'></div>
        </div>
-       <div className='dates-div overflow-hidden div-hover search-criteria' tabIndex={-1}>
+       <div className={`dates-div overflow-hidden div-hover search-criteria ${focusedIndex === 1 ? 'focused-div' : 'unfocused-div'}`}
+        onFocus={() => handleFocus(1)}
+        onBlur={handleBlur}
+        tabIndex={0}
+       >
         <p className='heading'>Check in</p>
         <p className='sub-heading'>Add dates</p>
        </div>
        <div >
         <div className='divider'></div>
        </div>
-       <div className='dates-div overflow-hidden div-hover search-criteria' tabIndex={-1}>
+       <div className={`dates-div overflow-hidden div-hover search-criteria ${focusedIndex === 2 ? 'focused-div' : 'unfocused-div'}`}
+        onFocus={() => handleFocus(2)}
+        onBlur={handleBlur}
+        tabIndex={0}
+       >
         <p className='heading'>Check out</p>
         <p className='sub-heading'>Add dates</p>
        </div>
        <div>
         <div className='divider'></div>
        </div>
-       <div className='fourth-div div-hover search-criteria' tabIndex={-1}>
-        <p className='heading'>Who</p>
-        <p className='sub-heading'>Add guests</p>
+       <div className={`fourth-div div-hover search-criteria ${focusedIndex === 3 ? 'focused-div' : 'unfocused-div'}`}
+        onFocus={() => handleFocus(3)}
+        onBlur={handleBlur}
+        tabIndex={0}
+       >
+        <div>
+         <p className='heading'>Who</p>
+         <p className='sub-heading'>Add guests</p>
+        </div>
+        <div className='search-img-container'>
+         <img className='search-img' src='/assets/asset 65.svg' />
+        </div>
        </div>
-       <div className='search-img-container'>
-        <img className='search-img' src='/assets/asset 65.svg' />
-       </div>
+
       </div>
      </div>
     </div>
--- frontend/src/components/StickyFooter/StickyFooter.tsx
+++ frontend/src/components/StickyFooter/StickyFooter.tsx
@@ -22,32 +22,44 @@ export default function StickyFooter() {
  }, [lastScrollY]);
 
  return (
-  <div className='mobile-footer-container'>
 
-   <button style={{ marginBottom: bottomMargin }} className={`map-bttn-container ${isScrollingDown ? "slide-in" : atTop ? "fade-out slide-out" : "stay-on-top"}`}>
-    <p>Map</p>
-    <img src='/assets/asset 72.svg' />
-   </button>
+  <div>
 
-   <div className={`sticky-footer-container ${isScrollingDown ? "slide-down" : atTop ? "slide-up" : ""}`}>
-    <ul>
-     <li>
-      <img src='/assets/big-search-glass.svg' />
-      <p>Explore</p>
-     </li>
-     <li>
-      <img src='/assets/wishlist.svg' />
-      <p>Wishlists</p>
-     </li>
-     <li>
-      <img src='/assets/log-in.svg' />
-      <p>Log in</p>
-     </li>
-    </ul>
+   <div className='mobile-footer-container'>
+
+    <button style={{ marginBottom: bottomMargin }} className={`map-bttn-container ${isScrollingDown ? "slide-in" : atTop ? "fade-out slide-out" : "stay-on-top"}`}>
+     <p>Map</p>
+     <img src='/assets/asset 72.svg' />
+    </button>
+
+
+
+    <div className={`sticky-footer-container ${isScrollingDown ? "slide-down" : atTop ? "slide-up" : ""}`}>
+     <ul>
+      <li>
+       <img src='/assets/big-search-glass.svg' />
+       <p>Explore</p>
+      </li>
+      <li>
+       <img src='/assets/wishlist.svg' />
+       <p>Wishlists</p>
+      </li>
+      <li>
+       <img src='/assets/log-in.svg' />
+       <p>Log in</p>
+      </li>
+     </ul>
+    </div>
+
+
    </div>
 
-
+   <button className={`map-bttn-mid-screen-size`}>
+    <p>Show map</p>
+    <img src='/assets/asset 72.svg' />
+   </button>
   </div>
+
 
  )
 }
